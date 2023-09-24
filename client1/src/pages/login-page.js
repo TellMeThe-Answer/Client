0f9e2b35b23a6dbca4101bba6058ea093a5670a4
18011879 <48465915+18011879@users.noreply.github.com>
2023-09-24 18:59:04 +0900

    login modify

--- client1/src/pages/login-page.js
+++ client1/src/pages/login-page.js
@@ -5,15 +5,28 @@ import { Navigate, useNavigate } from 'react-router-dom';
 // 모달 스타일을 LoginPage 컴포넌트 바깥에서 정의합니다.
 const modalStyle = {
   position: 'fixed',
-  top: 0,
-  left: 0,
+  top: '0',
+  left: '0',
   width: '100%',
   height: '100%',
-  backgroundColor: 'rgba(0,0,0,0.5)',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 };
+
+const modalContentStyle = {
+  backgroundColor: 'white',
+  padding: '20px',
+  borderRadius: '8px',
+};
+
+// 가상의 회원 정보
+const users = [
+  { email: 'user1@example.com', password: 'password1' },
+  { email: 'user2@example.com', password: 'password2' },
+  // 추가 회원 정보 추가 가능
+];
 
 function LoginPage() {
   const [email, setEmail] = useState('');
@@ -25,22 +38,39 @@ function LoginPage() {
 
   const handleLoginSubmit = (e) => {
     e.preventDefault();
-    console.log('로그인 이메일:', email);
-    console.log('로그인 비밀번호:', password);
-    // 로그인 로직을 여기에 추가하면 됩니다.
-    // 예를 들어, 서버로 이메일과 비밀번호를 전송하고 인증을 수행하는 등의 작업을 수행합니다.
-    // 로그인에 성공하면 setShouldNavigate(true);를 호출하여 페이지를 이동시킬 수 있습니다.
-    setShouldNavigate(true); // 임시로 true 설정
 
-    // 아래는 예시 코드로 로그인에 성공하면 메뉴 페이지로 이동하는 예시입니다.
-    // 실제 프로젝트에서는 서버와의 통신 등을 통해 로그인을 처리해야 합니다.
-    // if (로그인 성공) {
-    //   setShouldNavigate(true);
-    // }
+    // 입력값 검증
+    if (email.trim() === '' || password.trim() === '') {
+      // 이메일 또는 비밀번호가 비어있으면 로그인을 막습니다.
+      return;
+    }
+
+    // 입력한 이메일과 비밀번호와 일치하는 회원 정보 찾기
+    const user = users.find((user) => user.email === email && user.password === password);
+
+    if (user) {
+      // 회원 정보가 일치하면 로그인 성공
+      console.log('로그인 이메일:', email);
+      console.log('로그인 비밀번호:', password);
+      setShouldNavigate(true); // 로그인 성공 시 페이지 이동
+    } else {
+      // 회원 정보가 일치하지 않으면 로그인 실패
+      alert('이메일 또는 비밀번호가 올바르지 않습니다.');
+    }
   };
 
   const handleSignupSubmit = (e) => {
     e.preventDefault();
+
+    // 입력값 검증
+    if (signupEmail.trim() === '' || signupPassword.trim() === '') {
+      // 회원가입 이메일 또는 비밀번호가 비어있으면 회원가입을 막습니다.
+      return;
+    }
+
+    // 회원 가입 정보 추가
+    users.push({ email: signupEmail, password: signupPassword });
+
     console.log('회원가입 이메일:', signupEmail);
     console.log('회원가입 비밀번호:', signupPassword);
     setIsSignupModalOpen(false);
@@ -76,34 +106,36 @@ function LoginPage() {
 
       {isSignupModalOpen && (
         <div style={modalStyle}>
-          {/* 회원가입 모달 내용 */}
-          <h2>회원가입</h2>
-          <form onSubmit={handleSignupSubmit}>
-            <div style={{ marginBottom: '10px' }}>
-              <label>
-                이메일:
-                <input
-                  type="email"
-                  value={signupEmail}
-                  onChange={(e) => setSignupEmail(e.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <div style={{ marginBottom: '10px' }}>
-              <label>
-                비밀번호:
-                <input
-                  type="password"
-                  value={signupPassword}
-                  onChange={(e) => setSignupPassword(e.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <button type="submit">회원가입</button>
-          </form>
-          <button onClick={() => setIsSignupModalOpen(false)}>닫기</button>
+          <div style={modalContentStyle}>
+            {/* 회원가입 모달 내용 */}
+            <h2>회원가입</h2>
+            <form onSubmit={handleSignupSubmit}>
+              <div style={{ marginBottom: '10px' }}>
+                <label>
+                  이메일:
+                  <input
+                    type="email"
+                    value={signupEmail}
+                    onChange={(e) => setSignupEmail(e.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              <div style={{ marginBottom: '10px' }}>
+                <label>
+                  비밀번호:
+                  <input
+                    type="password"
+                    value={signupPassword}
+                    onChange={(e) => setSignupPassword(e.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              <button type="submit">회원가입</button>
+            </form>
+            <button onClick={() => setIsSignupModalOpen(false)}>닫기</button>
+          </div>
         </div>
       )}
     </div>
