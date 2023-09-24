13531bf8eb4edbf4b362e4de572286996cc312bb
18011879 <48465915+18011879@users.noreply.github.com>
2023-09-24 16:58:23 +0900

    login, sign up

---
+++ client1/src/pages/menu-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <button>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+      <button>Button 4</button>
+    </div>
+  );
+};
+
+export default MainPage;
