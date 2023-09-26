1a8237916f6b05e06cedfa5bd3f4e9ce11c2c862
18011879 <48465915+18011879@users.noreply.github.com>
2023-09-26 13:40:10 +0900

    image cropper

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,11 +1,12 @@
 // App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import MenuPage from './MenuPage';
-import DetectionPage from './DetectionPage';
-import DictionaryPage from './DictionaryPage';
-import MapPage from './MapPage';
-import MonthPage from './MonthPage';
+import LoginPage from './pages/login-page'; // Import LoginPage component
+import MenuPage from './pages/menu-page'; // Import MenuPage component
+import DetectionPage from './pages/detection-page'; // Import DetectionPage component
+import DictionaryPage from './pages/dictionary-page'; // Import DictionaryPage component
+import MapPage from './pages/map-page'; // Import MapPage component
+import MonthPage from './pages/month-page'; // Import MonthPage component
 
 function App() {
   return (
--- client1/src/pages/detection-page.js
+++ client1/src/pages/detection-page.js
@@ -1,19 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css'; // CropperJS의 스타일을 임포트합니다.
 
 const DetectionPage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const imageRef = useRef(null); // image element에 대한 참조를 생성합니다.
+  let cropper;
 
   const handleImageChange = (e) => {
-    const file = e.target.files[0]; // 첫 번째 선택한 파일 가져오기
+    const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = (e) => {
-        const imageDataUrl = e.target.result; // 이미지를 데이터 URL로 변환
+        const imageDataUrl = e.target.result;
         setSelectedImage(imageDataUrl);
       };
-      reader.readAsDataURL(file); // 파일을 데이터 URL로 읽기
+      reader.readAsDataURL(file);
     }
   };
+
+  useEffect(() => {
+    if (selectedImage) {
+      // 이미지가 선택되면 cropper 인스턴스를 생성합니다.
+      cropper = new Cropper(imageRef.current, {
+        aspectRatio: 16 / 9,
+        crop(event) {
+          console.log(event.detail.x);
+          console.log(event.detail.y);
+          console.log(event.detail.width);
+          console.log(event.detail.height);
+          console.log(event.detail.rotate);
+          console.log(event.detail.scaleX);
+          console.log(event.detail.scaleY);
+        },
+      });
+    }
+    return () => {
+      // 컴포넌트가 언마운트되면 cropper 인스턴스를 제거합니다.
+      if (cropper) {
+        cropper.destroy();
+      }
+    };
+  }, [selectedImage]); // selectedImage가 변경될 때마다 effect를 실행합니다.
 
   return (
     <div>
@@ -22,7 +50,8 @@ const DetectionPage = () => {
       {selectedImage && (
         <div>
           <h2>Selected Image:</h2>
-          <img src={selectedImage} alt="Selected" width="300" />
+          {/* 이미지 참조를 설정합니다. */}
+          <img ref={imageRef} src={selectedImage} alt="Selected" width="300" />
         </div>
       )}
     </div>
