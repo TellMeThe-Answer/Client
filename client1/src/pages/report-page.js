13d2f729b8e5781eae2abe99f8c4e9abc18b7074
18011879 <48465915+18011879@users.noreply.github.com>
2023-10-04 19:25:25 +0900

    신고하기페이지

--- client1/src/pages/report-page.js
+++ client1/src/pages/report-page.js
@@ -1,7 +1,36 @@
-import React, { useEffect } from 'react';
-import '../css/App.css'; // Adjust the path as per your project structure.
+import React, { useEffect, useState } from 'react';
+import '../css/App.css';
 import { BottomNavbar } from '../components/components';
+import Select from 'react-select';
+
 const ReportPage = () => {
+  const [userInput, setUserInput] = useState({
+    crop: null,
+    disease: null,
+    symptoms: '',
+  });
+
+  const [diseaseOptions, setDiseaseOptions] = useState([]);
+
+  const cropOptions = [
+    { value: 'crop1', label: 'Crop 1' },
+    { value: 'crop2', label: 'Crop 2' },
+    // Add other crops here...
+  ];
+
+  const allDiseaseOptions = {
+    crop1: [
+      { value: 'disease1', label: 'Disease 1 for Crop 1' },
+      { value: 'disease2', label: 'Disease 2 for Crop 1' },
+      // Add other diseases here...
+    ],
+    crop2: [
+      { value: 'disease3', label: 'Disease 1 for Crop 2' },
+      { value: 'disease4', label: 'Disease 2 for Crop 2' },
+      // Add other diseases here...
+    ],
+  };
+
   useEffect(() => {
     const script = document.createElement('script');
     script.type = 'text/javascript';
@@ -10,36 +39,84 @@ const ReportPage = () => {
 
     script.onload = () => {
       window.kakao.maps.load(() => {
-        // 사용자의 현재 위치를 얻기
         navigator.geolocation.getCurrentPosition((position) => {
-          const userLat = position.coords.latitude; // 사용자의 위도
-          const userLng = position.coords.longitude; // 사용자의 경도
-
+          const userLat = position.coords.latitude;
+          const userLng = position.coords.longitude;
           const container = document.getElementById('map__kakao');
           const options = {
-            center: new window.kakao.maps.LatLng(userLat, userLng), // 지도의 중심을 사용자의 위치로 설정
+            center: new window.kakao.maps.LatLng(userLat, userLng),
             level: 3,
           };
           const map = new window.kakao.maps.Map(container, options);
 
-          // 마커 생성
           const markerPosition = new window.kakao.maps.LatLng(userLat, userLng);
           const marker = new window.kakao.maps.Marker({ position: markerPosition });
 
-          // 지도 위에 마커를 표시
           marker.setMap(map);
         });
       });
     };
   }, []);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserInput((prevInput) => ({ ...prevInput, [name]: value }));
+  };
+
+  const handleCropChange = (selectedOption) => {
+    const selectedCrop = selectedOption ? selectedOption.value : null;
+    setUserInput((prevInput) => ({ ...prevInput, crop: selectedCrop, disease: null }));
+    setDiseaseOptions(selectedCrop ? allDiseaseOptions[selectedCrop] : []);
+  };
+
+  const handleDiseaseChange = (selectedOption) => {
+    const selectedDisease = selectedOption ? selectedOption.value : null;
+    setUserInput((prevInput) => ({ ...prevInput, disease: selectedDisease }));
+  };
+
+  const handleReportSubmit = () => {
+    // Handling the report submit. This can be sending the data to a server.
+    alert("Report Submitted:\n" + JSON.stringify(userInput, null, 2));
+  };
+
   return (
     <div>
-    <div style={{ padding: "40px" }}>
-      <h1>Map Page</h1>
-      <div id="map__kakao" style={{ width: '100%', height: '100vh' }}></div>
-    </div>
-    <BottomNavbar />
+      <div style={{ padding: "40px" }}>
+        <h1>Report Page</h1>
+        <div style={{ marginBottom: '20px' }}>
+          <label>Crop: </label>
+          <Select
+            name="crop"
+            options={cropOptions}
+            onChange={handleCropChange}
+          />
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <label>Disease: </label>
+          <Select
+            name="disease"
+            options={diseaseOptions}
+            onChange={handleDiseaseChange}
+            isDisabled={!userInput.crop}
+          />
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <label>Symptoms: </label>
+          <textarea
+            name="symptoms"
+            value={userInput.symptoms}
+            onChange={handleInputChange}
+          />
+        </div>
+        {/* Reduce the map size to 75% of the width and height */}
+        <div id="map__kakao" style={{ width: '75%', height: '35vh', margin: '0 auto' }}></div>
+        <div style={{ marginTop: '20px' }}>
+          <button onClick={() => alert('Report Submitted!')} style={{ padding: '10px 20px', fontSize: '16px' }}>
+            Report
+          </button>
+        </div>
+      </div>
+      <BottomNavbar />
     </div>
   );
 };
