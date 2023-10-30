25e747d1ec9e99d0edbef543b9c65ef63a89233e
18011879 <48465915+18011879@users.noreply.github.com>
2023-10-30 19:33:14 +0900

    병해도감(농약 제외)

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login-page';
 import MonthPage from './pages/month-page';
 import DetectionPage from './pages/detection-page';
@@ -8,24 +8,26 @@ import DictionaryPage from './pages/dictionary-page';
 import MapPage from './pages/map-page';
 import ReportPage from './pages/report-page';
 import MyPage from './pages/my-page';
+import CropListPage from './pages/CropListPage';
+
 function App() {  
   return(
     <Router>
-      <div className = "App">
+      <div className="App">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/month-page" element={<MonthPage />} />
           <Route path="/detection-page" element={<DetectionPage />} />
-          <Route path="/history-page" element = {<HistoryPage />} />
-          <Route path="/dictionary-page" element = {<DictionaryPage />} />
-          <Route path="/map-page" element = {<MapPage />} />
+          <Route path="/history-page" element={<HistoryPage />} />
+          <Route path="/dictionary-page" element={<DictionaryPage />} />
+          <Route path="/map-page" element={<MapPage />} />
           <Route path="/report-page" element={<ReportPage />} />
           <Route path="/my-page" element={<MyPage />} />
-          
+          <Route path="/croplist/:cropName" element={<CropListPage />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
