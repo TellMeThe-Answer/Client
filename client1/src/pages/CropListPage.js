04975d88a32480c7ec4d6b1d543ca6980ab40bd5
18011879 <48465915+18011879@users.noreply.github.com>
2023-11-06 23:43:04 +0900

    병해도감 상세 완료

--- client1/src/pages/CropListPage.js
+++ client1/src/pages/CropListPage.js
@@ -4,18 +4,72 @@ import { useParams } from 'react-router-dom';
 import { BigTitle } from "../components/components";
 import { FaArrowLeft } from 'react-icons/fa';
 import '../css/App.css';
+import xml2js from 'xml2js';
+import timers from 'timers-browserify';
+import buffer from 'buffer'
 
 const Modal = ({ isOpen, onClose, disease }) => {
+  const [diseaseDetail, setDiseaseDetail] = useState(null);
+
+  useEffect(() => {
+    const fetchDiseaseDetail = async () => {
+      if (!disease || !isOpen) return;
+
+      try {
+        const params = {
+          serviceCode: 'SVC05',
+          apiKey: '202389033d01e9a4d596531416fc83c32132',
+          sickKey: disease.sickKey,
+        };
+        const response = await axios.get('/npmsAPI/service', { params });
+        if (response.data) {
+          xml2js.parseString(response.data, (err, result) => {
+            if (!err) {
+              setDiseaseDetail(result);
+            } else {
+              console.error('Error parsing XML:', err);
+              setDiseaseDetail(null);
+            }
+          });
+        } else {
+          console.error("Invalid data structure for disease with sickKey:", disease.sickKey);
+          setDiseaseDetail(null);
+        }
+      } catch (err) {
+        console.error("Error fetching detail for disease", disease);
+      }
+    };
+
+    fetchDiseaseDetail();
+  }, [disease, isOpen]);
+
+  // HTML 문자열을 안전하게 JSX로 변환하는 함수
+  const renderHTML = (rawHTML) =>
+    React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <button onClick={onClose} className="modal-close-button">X</button>
-        <h2>{disease.sickNameKor} ({disease.sickNameEng})</h2>
-        <p>작물명: {disease.cropName}</p>
-        <p>병 한글명: {disease.sickNameKor}</p>
-        {/* 여기에 더 많은 정보를 추가할 수 있습니다. */}
+        {diseaseDetail ? (
+          <>
+            <h2>{diseaseDetail.service.sickNameKor} ({diseaseDetail.service.sickNameEng})</h2>
+            {/* 기존 데이터 출력 방식을 renderHTML 함수를 사용하여 변환 */}
+           
+        
+            
+            <p>병 한문명: {renderHTML(diseaseDetail.service.sickNameChn)}</p>
+           
+            <p>발생생태: {renderHTML(diseaseDetail.service.developmentCondition)}</p>
+            <p>병 증상: {renderHTML(diseaseDetail.service.symptoms)}</p>
+            <p>방제방법: {renderHTML(diseaseDetail.service.preventionMethod)}</p>
+            
+          </>
+        ) : (
+          <p>병 상세 정보를 불러오는 중...</p>
+        )}
       </div>
     </div>
   );
