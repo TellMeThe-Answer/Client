ba51761a4c0d7e27795052a5d4b7fdddebfda9e3
rong5026 <rong5026@naver.com>
2023-11-27 17:52:27 +0900

    [UPDATE] 스플레쉬 페이지

--- src/pages/BeforeLogin/FirstPage.js
+++ src/pages/BeforeLogin/FirstPage.js
@@ -8,11 +8,11 @@ const FirstPage = () =>{
         <div className="w-full h-full flex justify-center items-center firstpage-container">
             <div className = "w-full justify-center items-center">
                 <div className = "flex justify-center w-full">
-                    <img src = "/images/005.png"/>
+                    <img src = "/images/logo.png" className='firstpage-logo'/>
                 </div>
-                <div className = "text-2xl font-semibold flex justify-center mt-10">로그인 후 이용해주세요.</div>
+                <div className ="text-white text-xl font-semibold flex justify-center mt-10">로그인 후 이용해주세요.</div>
                 <div className = "flex justify-center w-full">
-                    <Link to='/login' className = "w-40 h-10 mt-10 bg-[#10b981] text-white rounded-lg flex justify-center items-center">로그인</Link>
+                    <Link to='/login' className = "w-40 h-10 mt-10 bg-[#10b981] text-white rounded-lg flex justify-center items-center text-xl">시작하기</Link>
                 </div>
             </div>
         </div>
