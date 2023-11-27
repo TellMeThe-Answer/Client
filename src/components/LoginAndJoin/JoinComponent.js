3869685706f793c1cbfa3d4b8a2bad588f502cd3
rong5026 <rong5026@naver.com>
2023-11-27 19:59:05 +0900

    [ADD] 로그인 로고

--- src/components/LoginAndJoin/JoinComponent.js
+++ src/components/LoginAndJoin/JoinComponent.js
@@ -55,7 +55,6 @@ const JoinComponent = () => {
                 </div>
             </div>
         </div>
-        <Footer/>
         </section>
     )
 }
--- src/components/LoginAndJoin/LoginComponent.js
+++ src/components/LoginAndJoin/LoginComponent.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Footer from "../common/footer"
 import {Link} from 'react-router-dom';
-import "./css/LoginComponent.css"
+import "./LoginComponent.css"
+
+    
 
 const LoginComponent = () => {
 
@@ -16,18 +18,48 @@ const LoginComponent = () => {
     };
 
     const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('폼 데이터:', formData);
-    localStorage.setItem("email", "iopp3423@gmail.com");
-    localStorage.setItem("password", "test1234");
+        e.preventDefault();
+        console.log('폼 데이터:', formData);
+        localStorage.setItem("email", "iopp3423@gmail.com");
+        localStorage.setItem("password", "test1234");
     };  
 
 
     return(
-        <section class="bg-gray-50 h-full flex items-center">
-        <div className="login-container">
-            sdf
-        </div>
+        <section class="bg-white h-full flex items-center">
+            <div id="loginform">
+                <div id="login-logo">
+                    <div className="login-image-container">
+                        <img className='login-logo-image' 
+                            src="/images/logo_rev.png" alt='로고사진'/>
+                    </div>
+                   
+                </div>
+                <div>
+                    <div className="row">
+                        <label>User Name</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="ex) abcd@naver.com"
+                            onChange={handleChange} 
+                            id="email"
+                        />
+                    </div>
+                    <div className="row">
+                        <label>Phone Number</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="ex) 01012345678"
+                            onChange={handleChange} 
+                            id="password"/>
+                    </div>
+                    <div id="login-button" className="row">
+                        <button className='login-button'  type="submit" onClick={handleSubmit}>미팅 참여</button>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
