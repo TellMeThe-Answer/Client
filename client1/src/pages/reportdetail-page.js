385d767d8470f488430b2aba758a0743a0243b0c
18011879 <48465915+18011879@users.noreply.github.com>
2023-12-03 22:06:53 +0900

    신고내역 css

--- client1/src/pages/reportdetail-page.js
+++ client1/src/pages/reportdetail-page.js
@@ -1,18 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import '../css/App.css'; // Make sure this path is correct for your project
 
-// Confirmation Modal Component
 const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null;
 
   return (
     <div className="modal">
       <div className="modal-content">
-        <p>정말 신고내역을 삭제하시겠습니까?</p>
-        <button onClick={onConfirm}>네</button>
-        <button onClick={onClose}>아니요</button>
+        <p>정말 신고 내역을 삭제하시겠습니까?</p>
+        <button onClick={onConfirm} className="confirm-yes">네</button>
+        <button onClick={onClose} className="confirm-no">아니요</button>
       </div>
     </div>
+  );
+};
+
+const Pagination = ({ reportsPerPage, totalReports, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalReports / reportsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        {pageNumbers.map(number => (
+          <li key={number} className='page-item'>
+            <a onClick={() => paginate(number)} href='#!' className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
@@ -21,7 +42,8 @@ const ReportDetailpage = () => {
   const [editIndex, setEditIndex] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [deletingIndex, setDeletingIndex] = useState(null);
-  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reportsPerPage] = useState(5);
 
   useEffect(() => {
     const loadedReports = JSON.parse(localStorage.getItem('reports')) || [];
@@ -39,26 +61,10 @@ const ReportDetailpage = () => {
   };
 
   const confirmDelete = () => {
-    deleteReport(deletingIndex);
-    closeDeleteModal();
-  };
-
-  const deleteReport = (indexToDelete) => {
-    const updatedReports = reports.filter((_, index) => index !== indexToDelete);
+    const updatedReports = reports.filter((_, idx) => idx !== deletingIndex);
     setReports(updatedReports);
     localStorage.setItem('reports', JSON.stringify(updatedReports));
-  };
-
-  const handleEditLocation = (index) => {
-    const report = reports[index];
-    navigate('/markerfix-page', { 
-      state: { 
-        reportIndex: index, 
-        reportLat: report.lat, 
-        reportLng: report.lng,
-        cropType: report.crop 
-      } 
-    });
+    closeDeleteModal();
   };
 
   const handleSymptomsChange = (e, index) => {
@@ -72,39 +78,52 @@ const ReportDetailpage = () => {
     setEditIndex(null);
   };
 
-  if (reports.length === 0) {
-    return <div>No reports found.</div>;
-  }
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
+
+ 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Report List</h1>
-      {reports.map((report, index) => (
-        <div key={index} style={{ marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>
-          <button onClick={() => openDeleteModal(index)} style={{ float: 'right' }}>X</button>
-          <h2>Report {index + 1}</h2>
-          <div><strong>Crop:</strong> {report.crop}</div>
-          <div><strong>Disease:</strong> {report.disease}</div>
-          {editIndex === index ? (
-            <input 
-              type="text" 
-              value={report.symptoms} 
-              onChange={(e) => handleSymptomsChange(e, index)} 
-            />
-          ) : (
-            <div><strong>Symptoms:</strong> {report.symptoms}</div>
-          )}
-          <div><strong>Address:</strong> {report.address}</div>
-          <div><strong>Date:</strong> {report.date}</div>
-          <button onClick={() => handleEditLocation(index)}>위치 수정하기</button>
-          <button onClick={() => setEditIndex(index)}>증상 다시 작성하기</button>
-          {editIndex === index && <button onClick={saveEditedReport}>저장하기</button>}
+    <div className="report-list">
+      <h1 className="header">신고 내역 조회</h1>
+      {reports.slice((currentPage - 1) * reportsPerPage, currentPage * reportsPerPage).map((report, index) => (
+        <div key={index} className={`report-item ${editIndex === index ? 'editing' : ''}`}>
+          <div className="report-item-content">
+            {/* Display details here */}
+            <div><strong>작물:</strong> {report.crop}</div>
+            <div><strong>병해:</strong> {report.disease}</div>
+            {editIndex === index ? (
+              <textarea
+                className="edit-input"
+                value={report.symptoms}
+                onChange={(e) => handleSymptomsChange(e, index)}
+              />
+            ) : (
+              <div><strong>증상:</strong> {report.symptoms}</div>
+            )}
+            <div><strong>주소:</strong> {report.address}</div>
+            <div><strong>날짜:</strong> {report.date}</div>
+          </div>
+          <div className="report-item-controls">
+            {editIndex === index ? (
+              <button onClick={() => saveEditedReport(index)} className="save-button">저장</button>
+            ) : (
+              <button onClick={() => setEditIndex(index)} className="edit-button">수정</button>
+            )}
+            <button onClick={() => openDeleteModal(index)} className="delete-button">
+              <FaTimes />
+            </button>
+          </div>
         </div>
       ))}
+      <Pagination 
+        reportsPerPage={reportsPerPage}
+        totalReports={reports.length}
+        paginate={paginate}
+      />
       <ConfirmationModal 
-        isOpen={isModalOpen} 
-        onClose={closeDeleteModal} 
-        onConfirm={confirmDelete} 
+        isOpen={isModalOpen}
+        onClose={closeDeleteModal}
+        onConfirm={() => confirmDelete(deletingIndex)}
       />
     </div>
   );
